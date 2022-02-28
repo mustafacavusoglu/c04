7e9e9ca30b2a6191f6ae0ac29ce6fec43a4434c9
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2022-02-28 12:55:11 +0300

    Add files via upload

---
+++ c4yukle/ex05/ft_atoi_base.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcavusog <mcavusog@student.42istanbul.com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 17:36:16 by mcavusog          #+#    #+#             */
+/*   Updated: 2022/02/24 17:36:46 by mcavusog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	check_base(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	while (base[i])
+	{
+		if (ft_strlen(base) < 2)
+			return (0);
+		if (base[i] == '-' || base[i] == '+'
+			|| (base[i] < 32 || base[i] > 126) || base[i] == ' '
+			|| base[i] == '\n' || base[i] == '\r' || base[i] == '\v'
+			|| base[i] == '\t' || base[i] == '\f')
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[i] == base[j++])
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	is_inside(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (c == base[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int			i;
+	int			neg;
+	long int	res;
+
+	i = 0;
+	res = 0;
+	neg = 1;
+	if (!check_base(base))
+		return (0);
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg = -neg;
+		i++;
+	}
+	while (is_inside(str[i], base) != -1)
+	{
+		res = (res * ft_strlen(base)) + (is_inside(str[i], base));
+		i++;
+	}
+	return (res * neg);
+}
